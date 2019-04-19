2ea39b62509416d7bd9cee16e4b3d063f8a51abe
childressdev <jcampbell@childressagency.com>
2019-04-19 16:01:38 -0400

    added

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var cssnano = require('gulp-cssnano');
+
+gulp.task('sass', function(){
+  return gulp.src('dev/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(cssnano())
+    .pipe(sourcemaps.write('../dev/maps'))
+    .pipe(gulp.dest('wp-theme-files'))
+});
+
+gulp.task('js', function(){
+  return gulp.src('dev/js/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(concat('custom-scripts.min.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('../../dev/maps',{includeContent:false, sourceRoot: 'wp-theme-files'}))
+    .pipe(gulp.dest('wp-theme-files/js'))
+});
+
+gulp.task('watch', function(){
+  gulp.watch('dev/scss/**/*.scss', gulp.series('sass'));
+  gulp.watch('dev/js/*.js', gulp.series('js'));
+});
+
+gulp.task('default', gulp.parallel(['js', 'sass', 'watch']));
