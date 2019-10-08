ab66935964b08f0516dd6afe85af30e503662e21
Mikhail <mmccarty@childressagency.com>
2019-10-07 23:43:33 -0400

    figure out how to do stripes with rounded blue backgrounds.

--- wp-theme-files/page.php
+++ wp-theme-files/page.php
@@ -6,4 +6,28 @@
     </div>
   </div>
 
+  <div class="stripe image-left">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">hello</div>
+        <div class="col-md-6">
+          <div class="stripe-blue-right"></div>
+          <div class="image"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="stripe image-right">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="stripe-blue-left"></div>
+          <div class="image"></div>
+        </div>
+        <div class="col-md-6">hello</div>
+      </div>
+    </div>
+  </div>
+
 <?php get_footer();
