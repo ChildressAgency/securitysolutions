80da9d76eca17105509ce72df2137dd49ebebb99
Mikhail <mmccarty@childressagency.com>
2019-10-09 22:16:16 -0400

    embed contact form

--- wp-theme-files/template-parts/contact.php
+++ wp-theme-files/template-parts/contact.php
@@ -1,3 +1,8 @@
-<div class="contact-stripe text-center py-5">
-  <div>contact form goes here</div>
-</div>
+<?php if (get_field("contact_form", "options")): ?>
+  <div class="contact-stripe text-center py-5">
+    <?php
+    $code = '[contact-form-7 id="' . get_field("contact_form", "options") . '"]';
+    echo do_shortcode($code);
+    ?>
+  </div>
+<?php endif;
