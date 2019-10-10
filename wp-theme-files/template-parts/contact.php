65b4f6079484bfe82881c9d2b7766c05de2b90d2
Mikhail <mmccarty@childressagency.com>
2019-10-09 22:45:59 -0400

    contact form styling

--- wp-theme-files/template-parts/contact.php
+++ wp-theme-files/template-parts/contact.php
@@ -1,8 +1,16 @@
 <?php if (get_field("contact_form", "options")): ?>
-  <div class="contact-stripe text-center py-5">
-    <?php
-    $code = '[contact-form-7 id="' . get_field("contact_form", "options") . '"]';
-    echo do_shortcode($code);
-    ?>
+  <div class="contact-stripe text-center pb-5 pt-4">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-4 col-sm-12">
+          <h2 class="my-4">Let Us Know How We Can Help</h2>
+
+          <?php
+          $code = '[contact-form-7 id="' . get_field("contact_form", "options") . '"]';
+          echo do_shortcode($code);
+          ?>
+        </div>
+      </div>
+    </div>
   </div>
 <?php endif;
