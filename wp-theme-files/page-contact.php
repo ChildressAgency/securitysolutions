51d62c83b2a84d4c8cc061098ebbb97d381e7a8d
Mikhail <mmccarty@childressagency.com>
2019-10-29 10:26:52 -0400

    make contact page text editable; collapse long email on small devices

--- wp-theme-files/page-contact.php
+++ wp-theme-files/page-contact.php
@@ -8,16 +8,20 @@
     <div class="container">
       <div class="row contact-info justify-content-center">
         <div class="col-sm-12 col-md-10 text-center">
-          <h2>Reach us by phone or through email.</h2>
+          <h2><?php the_field("contact_top") ?></h2>
 
           <h1>
-            <?php echo get_field("phone", "options") ?> |
-            <a href="mailto:<?php echo get_field("email", "options") ?>">
+            <?php echo get_field("phone", "options") ?>
+            <span class="d-none d-md-inline"> | </span>
+            <a href="mailto:<?php echo get_field("email", "options") ?>" class="d-none d-md-block">
               <?php echo get_field("email", "options") ?>
+            </a>
+            <a href="mailto:<?php echo get_field("email", "options") ?>" class="d-block d-md-none">
+              Email Us
             </a>
           </h1>
 
-          <h2>Or fill out our pre-generated form below and we will contact you back as soon as possible.</h2>
+          <h2><?php the_field("contact_bottom") ?></h2>
 
         </div>
       </div>
